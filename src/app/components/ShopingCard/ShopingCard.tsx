7d15851bf96692551844bb5d27ef679bc323df66
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-16 21:54:50 +0500

    ShopingCard

--- src/app/components/ShopingCard/ShopingCard.tsx
+++ src/app/components/ShopingCard/ShopingCard.tsx
@@ -7,56 +7,48 @@ import { useEffect, useState } from 'react'
 
 
 interface Product {
-  name : string,
-  _id:string,
-  price:number,
-  description:string
+  name: string,
+  _id: string,
+  price: number,
+  description: string
 }
 
-
-
 const ShopingCard = () => {
+  const [product, setProduct] = useState<Product[]>([])
+  const [arrayMethod, setArrayMethod] = useState<Product[]>([])
 
-  const [product,setProduct] = useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
       const products = await client.fetch('*[_type == "product"]')
-      // console.log("...",products[1])
       setProduct(products)
     }
-  
+
     fetchData()
-  },[])
+  }, [])
 
-
-  const arrayMethod:Product[] = [];
-
-  for (let i=0; i<12; i++){
-    
-    arrayMethod.push(product[i])
-  }
+  useEffect(() => {
+    // When the products are fetched, slice the first 12 items into arrayMethod
+    setArrayMethod(product.slice(0, 12))
+  }, [product])
 
   return (
     <div className='flex flex-col items-center mt-10 mb-5 max-w-[1440px]'>
       <h1 className={`${style.productHeading} pb-6`}>Our Products</h1>
 
       <div className={`grid grid-cols-3 gap-8 justify-center mx-auto ${style.smallSizeScreen}`}>
-        
-          {arrayMethod.length > 0 ? (
-
+        {arrayMethod.length > 0 ? (
           arrayMethod.map((product: Product) => (
-              <Card key={product?._id} id={product?._id} price={product?.price} discountPercentage={''} image={{
-                asset: {
-                  _ref: '',
-                  _type: ''
-                },
+            <Card key={product?._id} id={product?._id} price={product?.price} discountPercentage={''} image={{
+              asset: {
+                _ref: '',
                 _type: ''
-              }} description={product?.description} name={product?.name} bgDisc='blue'/> // Passing the product data to Card component
-            ))
-          ) : (
-            <p>Loading products...</p> // Display loading message while data is being fetched
-          )}
+              },
+              _type: ''
+            }} description={product?.description} name={product?.name} bgDisc='blue' />
+          ))
+        ) : (
+          <p>Loading products...</p>
+        )}
       </div>
 
       <div className='mt-6'>
