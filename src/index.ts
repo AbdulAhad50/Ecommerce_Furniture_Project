cc2a8f1ed5f695f5da2984f45d69824de3f68077
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-20 17:50:00 +0500

    final

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import Checkout from "./app/checkout/Checkout";
+import Intput from "./app/checkout/Intput";
+import Country from "./app/checkout/Country";
+import Cart from "./app/DesktopCart/DesktopBarCart";
+import Hero from "./app/components/HeroSection/Hero";
+import Achievement from "../Final_Hackathon/src/app/components/achievement/Achievement";
+import ShopingCard from "./app/components/ShopingCard/ShopingCard";
+import GridComponent from "./app/components/Furniture/Furniture";
+import Nav from "./app/components/Header/Nav";
+import Footer from "./app/components/Footer/Footer";
+import Card from "./app/components/Cards/Card";
+import Browse from "./app/components/BrowsFeatures/Browse";
+
+console.log(Cart,Country,Intput,Checkout,ShopingCard,GridComponent,Nav,Footer,Card,Browse,Achievement,Hero)
