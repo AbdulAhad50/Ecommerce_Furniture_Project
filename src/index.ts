37b36cc74d05cfea5d8a60679f37fd6c5684205e
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-20 18:28:48 +0500

    syncing

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import Hero from "./app/components/HeroSection/Hero";
 import Achievement from "../Final_Hackathon/src/app/components/achievement/Achievement";
 import ShopingCard from "./app/components/ShopingCard/ShopingCard";
 import GridComponent from "./app/components/Furniture/Furniture";
-import Nav from "./app/components/Header/Nav";
+import Nav from "./app/components/Header/Nav";  
+
 import Footer from "./app/components/Footer/Footer";
 import Card from "./app/components/Cards/Card";
 import Browse from "./app/components/BrowsFeatures/Browse";
