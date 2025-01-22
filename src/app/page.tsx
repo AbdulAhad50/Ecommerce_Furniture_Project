e905e85a217d98acd153ee5a33de873ec50252af
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-23 01:56:38 +0500

    Acnnc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Browse from "./component/BrowsFeatures/Browse";
 import GridComponent from "./component/Furniture/Furniture";
 import Hero from "./component/HeroSection/Hero";
 import ShopingCard from "./component/ShopingCard/ShopingCard";
-
+import Help from './component/Help/Ancc'
 
 
 
@@ -11,9 +11,13 @@ export default function Home() {
   
   return (
 
-      <div className="max-w-[1440px] mx-[auto]">
+      <div className="relative max-w-[1440px] mx-[auto]">
           <Hero/>
           <Browse/>
+          <div className="absolute top-20 left-20">
+          <Help/>
+
+          </div>
           <ShopingCard/>
           <GridComponent/>
       </div>
