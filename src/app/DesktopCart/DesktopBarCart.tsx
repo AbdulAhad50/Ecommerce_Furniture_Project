57a9d772e3c4570da0aa967cf0234740aa8b9cae
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-22 10:17:19 +0500

    aray

--- src/app/DesktopCart/DesktopBarCart.tsx
+++ src/app/DesktopCart/DesktopBarCart.tsx
@@ -11,7 +11,7 @@ const Cart = () => {
   const [dataFind, setDataFind] = useState([]);
   const { data, deleteProduct } = useContext(StoreData);
 
-  const nameArray = [];
+  const nameArray:string[] = [];
   const totalPriceDataRef = useRef(0);
 
   useEffect(() => {
