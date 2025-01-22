66e31d5aaec73f2964ec199a515e86303e995ad0
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-22 10:32:14 +0500

    name

--- src/app/[product]/BreadCrumbs.tsx
+++ src/app/[product]/BreadCrumbs.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/breadcrumb"
 import style from './product.module.css'
 
-const BreadCrumbs = ({name}) => {
+const BreadCrumbs = ({name}:{name:string}) => {
   return (
     <div className="max-w-[1440px] h-[100px] mx-[auto] flex items-center bg-[#F9F1E7] pl-[20px]">
           <Breadcrumb>
