262ae7f6686fa60f6b4e5f5533cac4d8afff2310
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-23 14:40:47 +0500

    sync

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import Cart from "./app/DesktopCart/DesktopBarCart";
 import Intput from './app/checkout/Intput'
 import Country from './app/checkout/Intput'
 import Checkout from './app/checkout/Intput'
-import Browse from "./app/component/BrowsFeatures/Browse";
-import Footer from "./app/component/Footer/Footer";
-import GridComponent from "./app/component/Furniture/Furniture";
-import Nav from "./app/component/Header/Nav";
-import Hero from "./app/component/HeroSection/Hero";
-import ShopingCard from "./app/component/ShopingCard/ShopingCard";
-import Achievement from "./app/component/achievement/Achievement";
+import Browse from "./component/BrowsFeatures/Browse";
+import Footer from "./component/Footer/Footer";
+import GridComponent from "./component/Furniture/Furniture";
+import Nav from "./component/Header/Nav";
+import Hero from "./component/HeroSection/Hero";
+import ShopingCard from "./component/ShopingCard/ShopingCard";
+import Achievement from "./component/achievement/Achievement";
+
 
 
 
