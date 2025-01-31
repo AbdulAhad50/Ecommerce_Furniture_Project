5ceea060b8257b1f71b2324b5894e904e9cd743b
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-02-01 01:38:35 +0500

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+import { useContext, useEffect } from 'react'
 import Achievement from '../component/achievement/Achievement'
 import HeroBanner from '../HeroBanner/HeroBanner'
 import Cart from './Cart'
+import { StoreData } from '../store/StoreContext'
 
 
 const page = () => {
+
+  let { GetUser } = useContext(StoreData);
+
+  useEffect(()=>{
+    GetUser();
+  },[])
 
   return (
     <div className={`max-w-[1440px] mx-auto`}>
